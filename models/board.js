f0822e7671d7a420fa56b3ba063703db79908349
mslaughter <melissaslaughter@me.com>
2016-11-22 16:53:39 -0500

    added schema for board model

---
+++ models/board.js
@@ -0,0 +1,103 @@
+// MongoDB data model for Board
+
+var mongoose = require('mongoose')
+
+var boardSchema = new mongoose.Schema({
+    moderator: { 
+        type: String,
+        required: [true, 'Needs a moderator']
+    },
+    ideas: {
+        type: [IdeaSchema],
+        default: []
+    },
+    date: { 
+        type: Date, 
+        default: Date.now 
+    },
+});
+
+var boardModel = mongoose.model('board', boardSchema);
+
+var Boards = (function(boardModel) {
+
+    var that = {};
+
+    // Exposed function that takes an board and a callback.
+    // Expects the board in the form of:
+    //   {'moderator': 'userId'}
+    //
+    // We put the board in the _store, (with the addition
+    // of a UUID and Date()). If error, we send an error message
+    // back to the router.
+    that.addBoard = function(board, callback) {
+        var board = new boardModel({
+            moderator: board.moderator,
+        });
+
+        board.save(function(err, newboard) {
+            if (err) callback({ msg: err});
+            callback(null, newboard);
+        });
+    }
+
+    // Exposed function that takes an boardId (as a string) and
+    // a callback.
+    //
+    // Returns an board if the board exists, otherwise an error.
+    that.findBoard = function(boardId, callback) {
+        if (boardId.match(/^[0-9a-fA-F]{24}$/)) {
+            boardModel.findOne({ _id: boardId }, function(err, result) {
+                if (err) {
+                    callback({ msg: err });
+                }
+                if (result !== null) {
+                    callback(null, result);
+                } else {
+                    callback({ msg: 'No such board!' });
+                }
+            });
+        } else {
+            callback({ msg: 'No such board!' });
+        }
+    }
+
+    // Exposed function that takes a boardId (as a string) and 
+    // a callback.
+    //
+    // If there are boards associated with the moderatorId, returns
+    // an array of board objects, otherwise an error.
+    that.findBoardsByModerator = function(moderatorId, callback) {
+        boardModel.find({ moderator: moderatorId }).sort('-date').exec(function(err, result) {
+            if (err) callback({ msg: err });
+            if (result.length > 0) {
+                callback(null, result);
+            } else {
+                callback({ msg: 'No such boards!'})
+            }
+        });
+    }
+
+    // Exposed function that takes an boardId and a callback.
+    //
+    // If the boardId exists, we delete the board corresponding to
+    // that Id in the _store. Otherwise, we return an error.
+    that.removeboard = function(boardId, callback) {
+        boardModel.findOne({ _id: boardId }, function(err, result) {
+            if (err) callback({ msg: err });
+            if (result !== null) {
+                result.remove();
+                callback(null);
+            } else {
+                callback({ msg: 'No such board!'});
+            }
+        });
+    }
+
+    Object.freeze(that);
+    return that;
+
+})(boardModel);
+
+module.exports = Boards;
+
